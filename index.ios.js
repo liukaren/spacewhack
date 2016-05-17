0e8e2d8eff0a765860803ce922b61697a662d2a3
Karen Liu <karenliu1@gmail.com>
2016-05-16 17:12:19 -0700

    Animate mole bopping.

--- index.ios.js
+++ index.ios.js
@@ -37,6 +37,8 @@ const MOLE_ANIMATION_MS = 500 // How long to animate entering / exiting
 const WORMHOLE_ANIMATION_MS = 1000 // How long to animate the wormhole opening and closing
 const MOLE_DELAY_MS = 300 // How long to wait between opening the wormhole and showing the mole
 
+const MOLE_FULL_SCALE = 0.8
+
 function getTileSize() {
     const { height, width } = Dimensions.get('window')
     return {
@@ -46,6 +48,11 @@ function getTileSize() {
 }
 
 class Mole extends Component {
+    constructor(props) {
+        super(props)
+        this.state = { isBopped: false }
+    }
+
     componentWillMount() {
         this.animValue = new Animated.Value(0)
         this.wormHoleAnimValue = new Animated.Value(0)
@@ -55,17 +62,35 @@ class Mole extends Component {
         this.animateMoleTimeout = setTimeout(Animated.spring(this.animValue, {
             toValue: 1, friction: 3
         }).start, MOLE_DELAY_MS)
+
+        // After a timeout, animate the mole away and then call the parent to remove it
+        this.removeTimeout = setTimeout(() => {
+            Animated.spring(this.animValue, { toValue: 0, friction: 3 })
+                .start(this.props.removeMole)
+        }, this.props.removeTimeoutMs)
     }
 
     componentWillUnmount() {
+        clearTimeout(this.removeTimeout)
         clearTimeout(this.animateMoleTimeout)
     }
 
+    onBop() {
+        // Clear the remove timeout because the bop will remove it
+        clearTimeout(this.removeTimeout)
+
+        this.setState({ isBopped: true })
+        Animated.timing(this.animValue, { toValue: 0, easing: Easing.easeIn })
+            .start(this.props.onBop)
+    }
+
     render() {
-        const image = require('./images/alien.png') // TODO: switch based on moleType
+        // TODO: switch based on moleType
+        const image = require('./images/alien.png')
+        const boppedImage = require('./images/alienBopped.png')
         const { tileWidth, tileHeight } = getTileSize()
 
-        return <TouchableWithoutFeedback onPress={ this.props.onBop }>
+        return <TouchableWithoutFeedback onPress={ this.onBop.bind(this) }>
             <View>
                 <Animated.Image source={ require('./images/wormhole.png') }
                                 style={{
@@ -91,23 +116,42 @@ class Mole extends Component {
                                         })
                                     }]
                                }} />
-                <Animated.Image source={ image }
-                                style={{
-                                    width: tileWidth,
-                                    height: tileHeight,
-                                    resizeMode: 'contain',
-                                    opacity: this.animValue.interpolate({
-                                        // Invisible until the start of the animation
-                                        inputRange: [0.1, 1],
-                                        outputRange: [0, 1]
-                                    }),
-                                    transform: [{
-                                        scale: this.animValue.interpolate({
-                                            inputRange: [0, 1],
-                                            outputRange: [0.5, 0.8]
-                                        })
-                                    }]
-                                }} />
+                { !this.state.isBopped &&
+                    <Animated.Image source={ image }
+                                    style={{
+                                        width: tileWidth,
+                                        height: tileHeight,
+                                        resizeMode: 'contain',
+                                        opacity: this.animValue.interpolate({
+                                            // Invisible until the start of the animation
+                                            inputRange: [0.1, 1],
+                                            outputRange: [0, 1]
+                                        }),
+                                        transform: [{
+                                            scale: this.animValue.interpolate({
+                                                inputRange: [0, 1],
+                                                outputRange: [0.5, MOLE_FULL_SCALE]
+                                            })
+                                        }]
+                                    }} /> }
+                { this.state.isBopped &&
+                    <Animated.Image source={ boppedImage }
+                            style={{
+                                width: tileWidth,
+                                height: tileHeight,
+                                resizeMode: 'contain',
+                                opacity: this.animValue.interpolate({
+                                    inputRange: [0, 1],
+                                    outputRange: [0, 1]
+                                }),
+                                transform: [{
+                                    scale: this.animValue.interpolate({
+                                        inputRange: [0, 0.5, 1],
+                                        outputRange: [0.5, 1.3, MOLE_FULL_SCALE]
+                                    })
+                                }]
+                            }} /> }
+
             </View>
         </TouchableWithoutFeedback>
     }
@@ -115,7 +159,9 @@ class Mole extends Component {
 Mole.propTypes = {
     moleData: PropTypes.shape({
         moleType: PropTypes.oneOf(Object.keys(MOLE_TYPES).map((t) => MOLE_TYPES[t])),
-        onBop: PropTypes.func
+        onBop: PropTypes.func,
+        removeMole: PropTypes.func, // Call this when animations are done to fully remove
+        removeTimeoutMs: PropTypes.number
     })
 }
 
@@ -166,16 +212,7 @@ class Game extends Component {
             return
         }
 
-        // After MOLE_DURATION_MS, remove the mole
-        const removeTimeout = setTimeout(() => {
-            this.state.board[position.row][position.col] = null
-            this.setState({ board: this.state.board })
-        }, MOLE_DURATION_MS)
-
-        this.state.board[position.row][position.col] = {
-            moleType: MOLE_TYPES.ALIEN,
-            removeTimeout // TODO: Clear all timeout on unmount
-        }
+        this.state.board[position.row][position.col] = { moleType: MOLE_TYPES.ALIEN }
     }
 
     step() {
@@ -186,10 +223,13 @@ class Game extends Component {
         this.stepTimeout = setTimeout(this.step.bind(this), timeUntilNextStep)
     }
 
+    removeMole(row, col) {
+        this.state.board[row][col] = null
+        this.setState({ board: this.state.board })
+    }
+
     onBop(row, col) {
         const mole = this.state.board[row][col]
-        clearTimeout(mole.removeTimeout)
-        // TODO: play some kind of bop animation here
         this.state.board[row][col] = null
         this.setState({
             board: this.state.board,
@@ -227,7 +267,9 @@ class Game extends Component {
                             { row.map((col, colIndex) => (
                                 <View style={ styles.col } key={ colIndex }>
                                     { col && <Mole moleData={ col }
-                                                   onBop={ () => { this.onBop(rowIndex, colIndex) } } /> }
+                                                   onBop={ () => { this.onBop(rowIndex, colIndex) } }
+                                                   removeMole={ () => { this.removeMole(rowIndex, colIndex) } }
+                                                   removeTimeoutMs={ MOLE_DURATION_MS } /> }
                                 </View>
                             )) }
                         </View>
