54087c110d01879fdcd9b902f28fe8063877ced0
Karen Liu <karenliu1@gmail.com>
2016-05-24 21:17:25 -0700

    Animate gaining a life.

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,16 +1,23 @@
 import React, { Component, PropTypes } from 'react'
 import {
+    Animated,
     Image,
     StyleSheet,
     Text,
     TouchableWithoutFeedback,
     View
 } from 'react-native'
+
 import * as Constants from '../constants.js'
 import Actions from '../flux/actions.js'
 import { dispatch } from '../flux/dispatcher.js'
 
 export default class NavBar extends Component {
+    constructor(props) {
+        super(props)
+        this.lifeAnimValue = new Animated.Value(1)
+    }
+
     onTogglePause() {
         if (this.props.isPaused) {
             dispatch({ type: Actions.RESUME_GAME })
@@ -19,13 +26,26 @@ export default class NavBar extends Component {
         }
     }
 
+    componentWillReceiveProps(nextProps) {
+        if (nextProps.numLives > this.props.numLives) {
+            // We gained a life. Pop in that bad boy
+            this.lifeAnimValue = new Animated.Value(0.1) // TODO: find out why 0.1 is necessary
+            Animated.spring(this.lifeAnimValue, { toValue: 1, friction: 3 }).start()
+        }
+    }
+
     render() {
         // Show lives with repeating hearts
         const lifeEls = []
         for (let i = 0; i < this.props.numLives; i++) {
-            lifeEls.push(<Image key={ i }
+            const isMostRecent = i === this.props.numLives - 1
+            lifeEls.push(
+                <Animated.Image key={ i }
                                 source={ require('../../images/heart.png') }
-                                style={ styles.lifeImage } />)
+                                style={ [styles.lifeImage, isMostRecent && {
+                                    transform: [{ scale: this.lifeAnimValue }]
+                                }] } />
+            )
         }
 
         return <View style={ styles.navBar }>
